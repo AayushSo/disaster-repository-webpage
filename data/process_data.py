57ea15dd340fbd0e65ce6bd5c7e6a84218945867
Aayush Soni <aayush.soni795@gmail.com>
2024-05-22 18:09:46 +0530

    Update to take care of case where db file exists

--- data/process_data.py
+++ data/process_data.py
@@ -47,7 +47,7 @@ def save_data(df, database_filename):
 	Output : NA
 	"""
 	engine = create_engine('sqlite:///{}.db'.format(database_filename))
-	df.to_sql(database_filename, engine, index=False)		
+	df.to_sql(database_filename, engine, index=False, if_exists='replace')		
 
 def main():
 	if len(sys.argv) == 4:
